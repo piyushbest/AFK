6069ce3ed05eecb1a0299c53a42ddf4291be24d2
Jan Leigh <40911019+A354-PH@users.noreply.github.com>
2019-04-17 11:55:20 +0800

    Update Main.php

--- src/Assassiner354/AFK/Main.php
+++ src/Assassiner354/AFK/Main.php
@@ -8,6 +8,7 @@ use pocketmine\Server;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat as TF;
 
 //Events
 use pocketmine\event\Listener;
@@ -31,3 +32,14 @@ class Main extends PluginBase implements Listener {
       unset($this->afk[array_search($name, $this->afk)]);
     }
   }
+  
+  public function onMove(PlayerMoveEvent $event) {
+    $player = $event->getPlayer();
+    $name = $player->getName();
+    if(in_array($name, $this->afk)) {
+      unset($this->afk[array_search($name, $this->afk)]);
+      $player->setDisplayName($name);
+      $player->sendMessage(TF::GREEN . "You are no longer AFK!");
+    }
+  }
+}
